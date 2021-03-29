cbd26c3f3afaec538ff406cc3c1c535e0e6c8581
kastalis <kastalis00@protonmail.com>
2021-03-29 23:39:06 +0200

    Added hills and stars

--- sketch_25032021.js
+++ sketch_25032021.js
@@ -7,13 +7,16 @@ let building;
 let wndw = [];
 const windowNumber = 3;
 let wndwHeight;
+let crystalsArray = [];
 
 function setup()
 {
     canvasSize = min(windowWidth, windowHeight)
     cnv = createCanvas(canvasSize, canvasSize );
     centerCanvas();
-    
+
+    angleMode(DEGREES);
+
     measureUnit = width / 250;
     wndwHeight = 7*measureUnit;
 
@@ -27,25 +30,36 @@ function setup()
         let j = i - windowNumber;
         wndw[i] = new Window(building.height/3  + j*60, "right", building);
     }
+
+    for (let i = 0; i < 40; i++)
+    {
+        crystalsArray.push(new Crystal);
+    }
+    console.log(crystalsArray)
   
 } 
 
 
 function draw()
 {
-    background(22, 123, 56);
-    
+    //background(color('#4287f5'));
+    background(color('#012247'))
+
+    crystalsArray.forEach((element) => 
+    {
+        element.render();
+    });
+
+    theHills();
+
     stroke('black');
     strokeWeight(2);
-    
     building.render();
 
     for (let i = 0; i < 2*windowNumber; i++)
     {
         wndw[i].render();
     }
-   
-   
 }
 
 class Building
@@ -78,15 +92,14 @@ class Building
 
     render()
     {
-        stroke('black');
-        strokeWeight(2);
-
         push();
             translate(width/2, height/2);
 
+            //left side of the building
+            stroke(color('#ebb31a'))
+            strokeWeight(1);
             fill('yellow')
 
-            //left side of the building
             beginShape();
                 vertex(this.buildingStartingPoint[0], this.buildingStartingPoint[1]);
                 vertex(this.pointB[0], this.pointB[1]);
@@ -95,6 +108,10 @@ class Building
             endShape(CLOSE);
 
             //right side of the building
+            stroke(color('#ebb31a'))
+            strokeWeight(1);
+            fill('yellow')
+
             beginShape();
                 vertex(this.buildingStartingPoint[0], this.buildingStartingPoint[1]);
                 vertex(this.pointE[0], this.pointE[1]);
@@ -102,8 +119,11 @@ class Building
                 vertex(this.pointD[0], this.pointD[1]);
             endShape();
             
-            fill('red')
             //roof left side
+            stroke(color('#6b000e'))
+            strokeWeight(1);
+            fill('red')
+
             beginShape();
                 vertex(this.pointC[0], this.pointC[1]);   
                 vertex(this.pointG[0], this.pointG[1]);
@@ -111,6 +131,10 @@ class Building
             endShape();
 
             //roof right side
+            stroke(color('#6b000e'))
+            strokeWeight(1);
+            fill('red')
+
             beginShape();
                 vertex(this.pointD[0], this.pointD[1]);
                 vertex(this.pointG[0], this.pointG[1]);
@@ -118,8 +142,10 @@ class Building
             endShape();
             
             //door
-            strokeWeight(2)
-            fill('brown');
+            stroke(color('#6b000e'))
+            strokeWeight(1);
+            fill('brown')
+
             beginShape();
                 curveVertex(this.pointI[0], this.pointI[1]);
                 curveVertex(this.pointI[0], this.pointI[1]);
@@ -167,10 +193,7 @@ class Window
     }
     
     render()
-    {
-        
-        
-        
+    { 
         noStroke()
         fill(color('#52e3de'));
         
@@ -191,6 +214,60 @@ class Window
         pop();
     }
 
+}
+
+class Crystal
+{
+    constructor()
+    {
+        this.circleSize = random(50,300);
+        this.crystalColor = random(['#ade8f4', '#48cae4', '#0077b6']);
+        this.x = random(canvasSize);
+        this.y = random(canvasSize);
+        this.id = random(1000)
+    }
+
+    render()
+    {
+        strokeWeight(0.7);
+        stroke(this.crystalColor);
+        noFill();
+
+        ellipse(this.x, this.y, this.circleSize);
+
+        for (let a = 0; a <= 360; a+= 30)
+        {
+            push()
+            translate(this.x, this.y)
+            let coords = findPointOnCircle(a, this.circleSize);
+            line(0, 0, coords[0], coords[1])
+            pop()
+        }
+    }
+
+}
+
+
+function theHills()
+{
+    stroke(color('#ebb31a'))
+    strokeWeight(5);
+    fill('yellow')
+    beginShape();
+        curveVertex(0, height/2 + height/15);
+        curveVertex(0, height/2 + height/15);
+        curveVertex(width - width/2, height/2 + height/8);
+        curveVertex(width/2 + width/4, height/2 + height/3.5);
+        curveVertex(width/2 + width/3, height/2 + height/3);
+        curveVertex(width/2 + width/2.5, height/2 + height/3);
+        curveVertex(width/2 + width/2, height/2 + height/3);
+        curveVertex(width, height/2 + height/2 - 500);
+        curveVertex(width, height/2 + height/2 - 500);
+        vertex(width, height);
+        vertex(width, height);
+        vertex(0, height);
+        vertex(0, height);
+    endShape();
 }
 
 function centerCanvas() 
@@ -226,4 +303,12 @@ function findPointOnLine(angularCoefficient, intercept, inputCoord, coordToFind)
     {
         return (inputCoord-intercept)/angularCoefficient;
     }
+}
+
+function findPointOnCircle(theta, diam)
+{
+    let coords = [];
+    coords[0] = sin(theta) * diam/2;
+    coords[1] = cos(theta) * diam/2;
+    return coords;
 }
